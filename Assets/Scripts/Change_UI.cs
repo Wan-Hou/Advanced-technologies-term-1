0ec8f52894aca6025902aba78dd5ef70f8bdbb06
Hou Wan <146784220+Wan-Hou@users.noreply.github.com>
2025-11-08 17:33:11 +0000

    Tutorial Prompt

--- Assets/Scripts/Change_UI.cs
+++ Assets/Scripts/Change_UI.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 public class Change_UI : MonoBehaviour
 {
     public string text_to_load;
-    public GameObject ui_panel;
+    public GameObject ui_panel, tutorial_panel;
     public TextMeshProUGUI ui_text_reference;
     public Sprite image_to_load;
     public Image ui_image_reference;
 
     public void LoadTextIntoUI()
     {
+        tutorial_panel.gameObject.SetActive(false);
         Debug.Log("Found");
         ui_text_reference.text = text_to_load;
         ui_image_reference.sprite = image_to_load;
